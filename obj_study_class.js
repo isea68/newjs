f3f2189a875d11a98212636384e4bc2f574ae561
leesh <isea68@gmail.com>
2025-10-30 17:09:07 +0900

    fit : JS function & Arduino

---
+++ obj_study_class.js
@@ -0,0 +1,10 @@
+export class Movie {
+    title="";
+    actor="";
+    story="";
+    constructor(title, actor, story) {
+        this.title=title;
+        this.actor=actor;
+        this.story=story;
+    }
+}
---
+++ report/consult_class.js
@@ -0,0 +1,12 @@
+export class Consult {
+    name="";
+    age="";
+    date="";
+    story="";
+    constructor(name, age, date, story) {
+        this.name=name;
+        this.age=age;
+        this.date=date;
+        this.story=story;
+    }
+}
